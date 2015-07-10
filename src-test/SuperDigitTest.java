49c64e6c1f95fc2366d6c3de634f710b53581f6d
Piotr Kalinowski <Piotr.Kalinowski@genesyslab.com>
2015-07-10 21:54:26 +0200

    New method with pre-calculated super digits for modulo 9 of number of digits.

--- src-test/SuperDigitTest.java
+++ src-test/SuperDigitTest.java
@@ -1,5 +1,7 @@
 import static org.junit.Assert.*;
 
+import java.util.Arrays;
+
 import org.junit.Test;
 
 
@@ -7,6 +9,24 @@ public class SuperDigitTest {
 	
 	private SuperDigit s = new SuperDigit();
 
+	@Test
+	public void testSuperDigitRecursiveSingle() {
+		assertEquals(0, s.superDigitRecursive(0));
+		assertEquals(9, s.superDigitRecursive(9));
+	}
+	@Test
+	public void testSuperDigitRecursiveShort() {
+		assertEquals(1, s.superDigitRecursive(10));
+		assertEquals(7, s.superDigitRecursive(25));
+	}
+	@Test
+	public void testSuperDigitRecursiveLong() {
+		assertEquals(9, s.superDigitRecursive(99));
+		assertEquals(3, s.superDigitRecursive(75));
+		assertEquals(6, s.superDigitRecursive(9573));
+		assertEquals(2, s.superDigitRecursive(98741));
+		assertEquals(1, s.superDigitRecursive(1111111111111111111L));
+	}
 	@Test
 	public void testSuperDigitSingle() {
 		assertEquals(0, s.superDigit(0));
@@ -24,5 +44,25 @@ public class SuperDigitTest {
 		assertEquals(6, s.superDigit(9573));
 		assertEquals(2, s.superDigit(98741));
 		assertEquals(1, s.superDigit(1111111111111111111L));
+	}
+	@Test
+	public void testSuperDigitBothMethods() {
+		assertBothMethods(1234567890L);
+		assertBothMethods(1111111111L);
+		assertBothMethods(2222222222L);
+		assertBothMethods(7777777777L);
+		assertBothMethods(110022233344445555L);
+	}
+	private void assertBothMethods(long number) {
+		assertEquals(s.superDigitRecursive(number),
+				s.superDigit(number));
+	}
+	@Test
+	public void testPrepareSuperDigitsForModulo() {
+		int[][] result = s.prepareSuperDigitsForModulo();
+		for (int[] superDigits : result) {
+			System.out.println(Arrays.toString(superDigits));
+		}
+		
 	}
 }
--- src/SuperDigit.java
+++ src/SuperDigit.java
@@ -1,11 +1,79 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 
 public class SuperDigit {
+	/**
+	 * Faster method of calculating super digit. Uses an array of pre-calculated super digits
+	 * for numbers of digits modulo 9. Can be easily modified to accept BigInteger or another
+	 * type.
+	 */
 	int superDigit(long number) {
+		// super digits for sequences of same digits % 9
+		int[][] superDigits = prepareSuperDigitsForModulo();
+		List<Integer> digits = longToDigitArray(number);
+		int[] digitCount = countDigits(digits);
+		// sum of super digits for all distinct digits
+		int sum = 0;
+		for (int digit = 1; digit < 10; digit ++) {
+			int count = digitCount[digit];
+			int superDigit;
+			if (count == 0) {
+				superDigit = 0;
+			} else {
+				superDigit = superDigits[digit][count % 9];
+			}
+			sum += superDigit;
+		}
+		return superDigitRecursive(sum);
+	}
+	
+	int[][] prepareSuperDigitsForModulo() {
+		int[][] result = new int[10][9];
+		Arrays.fill(result[0], 0);
+		for (int digit = 1; digit < 10; digit ++) {
+			result[digit][0] = 9;
+			for (int modulo = 1; modulo < 9; modulo ++) {
+				result[digit][modulo] = superDigitRecursive(digit * modulo);
+			}
+		}
+		return result;
+	}
+
+	private int[] countDigits(List<Integer> digits) {
+		int[] digitCount = new int[10];
+		Arrays.fill(digitCount, 0);
+		for (Integer digit : digits) {
+			digitCount[digit]++;
+		}
+		return digitCount;
+	}
+
+	private List<Integer> longToDigitArray(long number) {
+		ArrayList<Integer> digits = new ArrayList<>();
+		do {
+			int digit = (int) (number % 10);
+			digits.add(digit);
+			number /= 10;
+		} while (number > 0);
+		return digits;
+	}
+
+	/**
+	 * Recursive algorithm that calculates super digit. 
+	 * @param number
+	 * @return
+	 */
+	int superDigitRecursive(long number) {
 		if (number < 10) {
 			return (int) number;
 		}
 		long sum = sumOfDigits(number);
-		return superDigit(sum);
+		return superDigitRecursive(sum);
 	}
 
 	private long sumOfDigits(long number) {
@@ -16,5 +84,19 @@ public class SuperDigit {
 			number /= 10;
 		} while (number > 0);
 		return sum;
+	}
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader reader = new BufferedReader(
+				new InputStreamReader(System.in));
+		SuperDigit instance = new SuperDigit();
+		while(true) {
+			System.out.print("Number: ");
+			String line = reader.readLine();
+			long number = Long.parseLong(line);
+			System.out.println("Super digit: " + instance.superDigit(number));
+		}
+
+
 	}
 }
